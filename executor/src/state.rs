96d254a27353a8ab353db10f93957da467150eec
1034701377-web <1034701377@qq.com>
2026-05-21 12:16:38 +0800

    extract action, implemented a task orchestration framework

--- executor/src/state.rs
+++ executor/src/state.rs
@@ -1,3 +1,5 @@
+use crate::action::Action;
+
 #[derive(Default, Copy, Clone)]
 pub(crate) struct State {
     pub(crate) is_reverse: bool,
@@ -6,5 +8,39 @@ pub(crate) struct State {
 impl State {
     pub(crate) fn be_reverse(&mut self) {
         self.is_reverse = !self.is_reverse;
+    }
+
+    pub(crate) fn assemble(&self, cmd: char) -> Vec<Action> {
+        match cmd {
+            'M' => self.move_assemble(),
+            'L' => self.turn_left_assemble(),
+            'R' => self.turn_right_assemble(),
+            _ => Vec::new(),
+        }
+    }
+
+    fn move_assemble(&self) -> Vec<Action> {
+        let direction = if self.is_reverse { -1 } else { 1 };
+        vec![Action::Forward(direction)]
+    }
+
+    fn turn_left_assemble(&self) -> Vec<Action> {
+        let turn_action = if self.is_reverse {
+            Action::TurnRight
+        } else {
+            Action::TurnLeft
+        };
+
+        vec![turn_action]
+    }
+
+    fn turn_right_assemble(&self) -> Vec<Action> {
+        let turn_action = if self.is_reverse {
+            Action::TurnLeft
+        } else {
+            Action::TurnRight
+        };
+
+        vec![turn_action]
     }
 }
