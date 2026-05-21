ceae1c1a4b7c1aa78051329265838c7828ad1bd5
1034701377-web <1034701377@qq.com>
2026-05-21 12:14:20 +0800

    extract forward turn_left turn_right to Pose

---
+++ executor/src/pose.rs
@@ -0,0 +1,52 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Pose {
+    pub x: i32,
+    pub y: i32,
+    pub heading: char,
+}
+
+impl Pose {
+    pub fn new(x: i32, y: i32, heading: char) -> Self {
+        Pose { x, y, heading }
+    }
+
+    pub(crate) fn forward(&mut self, offset: i32) {
+        match self.heading {
+            'E' => self.x += offset,
+            'S' => self.y -= offset,
+            'W' => self.x -= offset,
+            'N' => self.y += offset,
+            _ => (),
+        }
+    }
+
+    pub(crate) fn turn_left(&mut self) {
+        match self.heading {
+            'E' => self.heading = 'N',
+            'S' => self.heading = 'E',
+            'W' => self.heading = 'S',
+            'N' => self.heading = 'W',
+            _ => (),
+        }
+    }
+
+    pub(crate) fn turn_right(&mut self) {
+        match self.heading {
+            'E' => self.heading = 'S',
+            'S' => self.heading = 'W',
+            'W' => self.heading = 'N',
+            'N' => self.heading = 'E',
+            _ => (),
+        }
+    }
+}
+
+impl Default for Pose {
+    fn default() -> Self {
+        Pose {
+            x: 0,
+            y: 0,
+            heading: 'N',
+        }
+    }
+}
